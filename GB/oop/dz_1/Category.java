0b2cae531c32ff195ef5a3ebfb73846ea60ed767
Yarkin Alexey <aarkin002@gmail.com>
2023-03-31 10:00:46 +0300

    DZ_1 уже проверенное Преподователем.

---
+++ GB/oop/dz_1/Category.java
@@ -0,0 +1,38 @@
+package GB.oop.dz_1;
+
+import java.util.ArrayList;
+
+public class Category {
+    private String cat_name;
+    private ArrayList<Product> cat_products;
+
+    public Category(String name, ArrayList<Product> cat_products) {
+        this.cat_name = name;
+        this.cat_products = cat_products;;
+    }
+
+    public Category() {
+    }
+
+    public String getCat_name() {
+        return cat_name;
+    }
+
+    public void setCat_name(String cat_name) {
+        this.cat_name = cat_name;
+    }
+
+    public ArrayList<Product> getCat_products() {
+        return this.cat_products;
+    }
+
+    public void addCat_products(Product product) {
+        this.cat_products.add(product);
+    }
+
+    public void printCat_products(){
+        for (Product catProduct : getCat_products()) {
+            System.out.printf("%s price: %s rating: %s количество: %s%n", catProduct.getName(), catProduct.getPrice(), catProduct.getRating(), catProduct.getCnt());
+        }
+    }
+}
---
+++ GB/oop/dz_1/Main.java
@@ -0,0 +1,53 @@
+package GB.oop.dz_1;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /** Создаем продукты, категории, баскеты и пользователей */
+
+        Product hp = new Product("HP", 21000, 4, 14);
+        Product lenovo = new Product("LENOVO", 11000, 3, 7);
+        Product sony = new Product("Sony", 17000, 2, 11);
+        ArrayList<Product> nout_arr = new ArrayList<>();
+        ArrayList<Product> mon_arr = new ArrayList<>();
+        ArrayList<Product> sw_arr = new ArrayList<>();
+        Category noutbooks = new Category("n2016", nout_arr);
+        Category monitors = new Category("17", mon_arr);
+        Category swithes = new Category("smart", sw_arr);
+        noutbooks.addCat_products(hp);
+        noutbooks.addCat_products(lenovo);
+        noutbooks.addCat_products(sony);
+        ArrayList<Product> usr1_bsk_arr = new ArrayList<>();
+        Basket usr1_bsk = new Basket(usr1_bsk_arr);
+        User usr1 = new User("usr1", "pswrd1", usr1_bsk);
+        ArrayList<Product> usr2_bsk_arr = new ArrayList<>();
+        Basket usr2_bsk = new Basket(usr1_bsk_arr);
+        User usr2 = new User("usr2", "pswrd2", usr2_bsk);
+        ArrayList<Product> usr3_bsk_arr = new ArrayList<>();
+        Basket usr3_bsk = new Basket(usr1_bsk_arr);
+        User usr3 = new User("usr3", "pswrd3", usr3_bsk);
+        //Выводим в консоль содержимое категории
+        System.out.println("Изначальное содержимое категории:");
+        noutbooks.printCat_products();
+
+        /** Пользователь покупает продукт */
+
+        usr1.addUserBasket(hp);
+        usr1.addUserBasket(hp);
+        usr1.addUserBasket(lenovo);
+        usr1.addUserBasket(sony);
+
+        /** Выводим в консоль актуальное содержимое категории */
+
+
+        System.out.println("Содержимое категории после покупки пользователем:");
+        noutbooks.printCat_products();
+
+        /** Выводим в консоль содержимое Basket */
+
+        System.out.println("Содержимое пользовательского Basket:");
+        usr1.printUser_basket();
+    }
+}
